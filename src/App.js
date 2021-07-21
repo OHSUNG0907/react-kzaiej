44f32fcd82264a85bafe1305fe43c2f57b35cd6c
OHSUNG0907 <oohs0907@outlook.com>
2021-07-22 00:23:34 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, text: 'Wash dishes', done: false },
     { id: 2, text: 'Do laundry', done: false },
     { id: 3, text: 'Take shower', done: false }
-  ];
+  ]);
 
   return (
     <div>
       <h1>Todo List</h1>
 
       <TodoList todos={todos} />
-      <AddTodo />
+      <AddTodo setTodos={setTodos} />
     </div>
   );
 }
@@ -28,11 +28,35 @@ function TodoList({ todos }) {
   );
 }
 
-function AddTodo() {
+function AddTodo({ setTodos }) {
+  const inputRef = React.useRef();
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: 4,
+      text,
+      done: false
+    };
+    console.log(todo);
+    setTodos(prevTodos => {
+      return prevTodos.concat(todo);
+    });
+    inputRef.current.value = '';
+  }
+
   return (
-    <form>
-      <input placeholder="Add todo" />
+    <form onSubmit={handleAddTodo}>
+      <input placeholder="Add todo" name="addTodo" ref={inputRef} />
       <br />
+      <select name="symbol">
+        <option selected value="BTC">
+          Bitcoin
+        </option>
+        <option value="ETH">Ethereum</option>
+        <option value="BNB">Binance Coin</option>
+        <option value="ADA">Cardano</option>
+      </select>
       <button type="submit">Submit</button>
     </form>
   );
