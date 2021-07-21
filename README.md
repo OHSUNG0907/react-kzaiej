265e8e5a552067b6002ed00704bc39a511fae6b9
OHSUNG0907 <oohs0907@outlook.com>
2021-07-21 20:21:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kzaiej
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kzaiej)
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  const todos = [
+    { id: 1, text: 'Wash dishes', done: false },
+    { id: 2, text: 'Do laundry', done: false },
+    { id: 3, text: 'Take shower', done: false }
+  ];
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+function TodoList(props) {
+  return (
+    <ul>
+      {props.todos.map(todo => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+}
